1c76f50b42b967154f5becb88f030b28964c4bae
rdcgomez <zemogzner@gmail.com>
2018-04-16 17:01:04 +0800

    Restart Everything Again

--- AndroidStudioProjects/Sportstation/sportstationapp/src/main/java/com/project/apc/sportstation/sportstationapp/LoginActivity.java
+++ AndroidStudioProjects/Sportstation/sportstationapp/src/main/java/com/project/apc/sportstation/sportstationapp/LoginActivity.java
@@ -3,7 +3,7 @@ package com.project.apc.sportstation.sportstationapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainSportstation extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
