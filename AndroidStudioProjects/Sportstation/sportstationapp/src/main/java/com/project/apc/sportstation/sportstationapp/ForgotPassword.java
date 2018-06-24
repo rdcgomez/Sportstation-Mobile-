08d560f23afccd1bb0bb0b3768ad6c53c749dd95
rdcgomez <zemogzner@gmail.com>
2018-06-24 17:12:10 +0800

    added onCreate in ForgotPassword.java

--- AndroidStudioProjects/Sportstation/sportstationapp/src/main/java/com/project/apc/sportstation/sportstationapp/ForgotPassword.java
+++ AndroidStudioProjects/Sportstation/sportstationapp/src/main/java/com/project/apc/sportstation/sportstationapp/ForgotPassword.java
@@ -1,7 +1,11 @@
 package com.project.apc.sportstation.sportstationapp;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ForgotPassword extends Activity{
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
 }
